212b8e845a3d746d59e5af7e521e86af77f483f5
Jon Bjornstad <jonb@logicalpoetry.com>
2022-07-14 01:27:06 -0700

    status.pl - seems right.  more testing tomorrow

--- nytbee/status.pl
+++ nytbee/status.pl
@@ -134,25 +134,32 @@ $y += 4;
 $y += $between;
 
 print {$out} "<text x=50 y=$y class=lets>S</text>\n";
-$y -=4;
+
+# a black line from 0 to max_score
+$y -=5; # centered on the S
 my $max_x = 75 + ($nwords-1)*7;
-my $x2 = $max_x * ($score/$max_score);
-print {$out} "<line x1=$x2 y1=$y x2=$max_x y2=$y stroke=black stroke-width=2></line>\n";
-my $y1 = $y-5;
-my $y2 = $y+5;
-my $max_x2 = 75 + ($score/$max_score)*($max_x);
+my $x1 = 75;
+print {$out} "<line x1=$x1 y1=$y x2=$max_x y2=$y stroke=black stroke-width=2></line>\n";
+
+# colored ranks between the percentages
+# but only up to the score %
 my @pct = (0, 2, 5, 9, 15, 25, 40, 50, 70, 100);
+my $score_pct = ($score/$max_score)*100;
 PCT:
 for my $i (0 .. $#pct-1) {
     my $x1 = 75 + ($pct[$i]/100)*($max_x - 75);
-    my $x2 = 75 + ($pct[$i+1]/100)*($max_x - 75);
-    if ($x2 > $max_x2) {
-        $x2 = $max_x2;
+    if ($score_pct < $pct[$i+1]) {
+        my $x2 = 75 + ($score_pct/100)*($max_x - 75);
         print {$out} "<line x1=$x1 y1=$y x2=$x2 y2=$y stroke='#$color[$i]' stroke-width=6></line>\n";
         last PCT;
     }
+    my $x2 = 75 + ($pct[$i+1]/100)*($max_x - 75);
     print {$out} "<line x1=$x1 y1=$y x2=$x2 y2=$y stroke='#$color[$i]' stroke-width=6></line>\n";
 }
+
+# vertical marks between ranks
+my $y1 = $y-5;
+my $y2 = $y+5;
 for my $pct (@pct) {
     my $x1 = 75 + ($pct/100)*($max_x - 75);
     my $x2 = $x1;
