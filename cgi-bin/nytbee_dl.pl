d85ec4164af6cbe96ad7c720796818664a9abd48
Jon Bjornstad <jon.bjornstad@gmail.com>
2024-04-02 14:50:35 -0700

    DL command complete - needs doc

--- cgi-bin/nytbee_dl.pl
+++ cgi-bin/nytbee_dl.pl
@@ -1,6 +1,12 @@
 #!/usr/bin/perl
+# messy as I copy/pasted
+# it works. :)
 use warnings;
 use strict;
+use Date::Simple qw/
+    today
+/;
+my $today_d8 = today()->as_d8();
 use BeeUtil qw/
     slash_date
     word_score
@@ -12,7 +18,6 @@ tie %cur_puzzles_store, 'DB_File', 'cur_puzzles_store.dbm';
 my %nyt_puzzles;
 tie %nyt_puzzles, 'DB_File', 'nyt_puzzles.dbm';
 
-print "The many games of $screen_name:<p>\n";
 my %cur_puzzles = %{ eval $cur_puzzles_store{$uuid} };
 
 my $ext_sig = '!*+-';   # extra word sigils
@@ -47,18 +52,25 @@ sub compute_score_and_rank {
         $rank_name = 'Queen Bee';
     }
 }
+my $list = "$screen_name-$today_d8-list.csv";
+my $full = "$screen_name-$today_d8-full.csv";
+open my $out1, '>', "../nytbee/downloads/$list";
+print {$out1} "date,rank,all_pangrams\n";
+open my $out2, '>', "../nytbee/downloads/$full";
+print {$out2} "date,letters,center,rank,all_pangrams,puzzle,donut,lexicon,bonus,stash\n";
 for my $dt (sort keys %cur_puzzles) {
-    print slash_date($dt);
     # get the allowed list and see if all
     # of the pangrams have been found
     # split on pipe, shift two
     my ($s, $t) = split m{[|]}xms, $nyt_puzzles{$dt};
+    my ($letters, $center) = split ' ', $s;
     my @ok_words = split ' ', $t;
     my @w = split ' ', $s;
     shift @w; shift @w;
     my $np = @w;
     %is_pangram = map { $_ => 1 } @w;
-    @found = grep { !/\A([0-9]|$ext_sig\z)/ } split ' ', $cur_puzzles{$dt};
+    my @words = grep { !/\A-?[0-9]/xms } split ' ', $cur_puzzles{$dt};
+    @found = grep { !/[$ext_sig]\z/xms } @words;
     $max_score = 0;
     for my $w (@ok_words) {
         $max_score += word_score($w, $is_pangram{$w});
@@ -77,6 +89,40 @@ for my $dt (sort keys %cur_puzzles) {
     );
     my $npf = grep { $is_pangram{$_} } @found;
     compute_score_and_rank();
-    my $p = $np == $npf? 'p': '';
-    print " $rank_name $p<br>\n";
+    my $p = $np == $npf? 'p': 'n';
+    print {$out1} "$dt,$rank_name,$p\n";
+    print {$out2} "$dt,$letters,$center,$rank_name,$p,";
+    my (@puzzle, @donut, @bonus, @lexicon, @stash);
+    for my $w (sort @words) {
+        if ($w !~ s{([$ext_sig])\z}{}xms) {
+            push @puzzle, $w;
+        }
+        else {
+            my $c = $1;
+            # ! stash * bonus + lexicon - donut
+            if ($c eq '!') {
+                push @stash, $w;
+            }
+            elsif ($c eq '*') {
+                push @bonus, $w;
+            }
+            elsif ($c eq '+') {
+                push @lexicon, $w;
+            }
+            elsif ($c eq '-') {
+                push @donut, $w;
+            }
+        }
+    }
+    print {$out2} "@puzzle,";
+    print {$out2} "@donut,";
+    print {$out2} "@lexicon,";
+    print {$out2} "@bonus,";
+    print {$out2} "@stash\n";
 }
+close $out1;
+close $out2;
+print "Links to download your puzzle files:<p>";
+print "<a style='margin-left: 1in' class=alink href='https://logicalpoetry.com/nytbee/downloads/$list' download>List</a>";
+print "&nbsp;" x 6;
+print "<a class=alink href='https://logicalpoetry.com/nytbee/downloads/$full' download>Full</a>";
