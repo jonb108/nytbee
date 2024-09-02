929e98d258134decf6b87e9a72144b7ceb6f74b5
Jon Bjornstad <jon.bjornstad@gmail.com>
2024-09-02 12:19:12 -0700

    redirects from logicalpoetry are complete

---
+++ cgi-bin/rd_nytbee.pl
@@ -0,0 +1,18 @@
+#!/usr/bin/env perl
+use strict;
+use warnings;
+use CGI;
+my $q = CGI->new();
+my $uuid = $q->cookie('uuid');
+# redirect to ultrabee.org
+print $q->header();
+print <<"EOF";
+<html>
+<head>
+<meta http-equiv="refresh" 
+      content="0; URL=https://ultrabee.org/cgi-bin/nytbee.pl/ID$uuid" />
+</head>
+<body>
+</body>
+</html>
+EOF
