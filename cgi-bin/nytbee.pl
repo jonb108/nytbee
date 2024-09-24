aa9c89d0d562f8088f96bf00d141a10ce1e4d7fb
Jon Bjornstad <jon.bjornstad@gmail.com>
2024-09-24 13:52:16 -0700

    favicon

--- cgi-bin/nytbee.pl
+++ cgi-bin/nytbee.pl
@@ -3971,6 +3971,10 @@ my $title_date = $date =~ m{\A CP}xms? $date
 print <<"EOH";
 <html>
 <head>
+<link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+<link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+<link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+<link rel="manifest" href="/site.webmanifest">
 <meta charset='UTF-8'>
 <title>UltraBee - $title_date</title>
 <style>
